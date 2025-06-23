20e79fbeb89ba16016582ce4450755fece1393a2
Monzirmajdi <bob121953@gmail.com>
2025-06-23 11:53:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,27 @@ document.addEventListener("DOMContentLoaded", () => {
                     previewImage: "images/social/1000003401.jpg",
                     images: ["images/social/1000003401.jpg", "images/social/1000003324.png", "images/social/1000003399.jpg", "images/social/1000003398.jpg", "images/social/1000003397.jpg", "images/social/1000003400.jpg"]
                 },
+                {
+                    title: "Baseline Medical - Eid Mubarak Campaign",
+                    description: "Eid greeting social media post for Baseline Medical with mosque imagery",
+                    tools: "Adobe Illustrator, Adobe Photoshop",
+                    previewImage: "images/social/1000003401.jpg",
+                    images: ["images/social/1000003401.jpg", "images/social/1000003324.png", "images/social/1000003399.jpg", "images/social/1000003398.jpg", "images/social/1000003397.jpg", "images/social/1000003400.jpg"]
+                },
+                {
+                    title: "Baseline Medical - Eid Mubarak Campaign",
+                    description: "Eid greeting social media post for Baseline Medical with mosque imagery",
+                    tools: "Adobe Illustrator, Adobe Photoshop",
+                    previewImage: "images/social/1000003401.jpg",
+                    images: ["images/social/1000003401.jpg", "images/social/1000003324.png", "images/social/1000003399.jpg", "images/social/1000003398.jpg", "images/social/1000003397.jpg", "images/social/1000003400.jpg"]
+                },
+                {
+                    title: "Baseline Medical - Eid Mubarak Campaign",
+                    description: "Eid greeting social media post for Baseline Medical with mosque imagery",
+                    tools: "Adobe Illustrator, Adobe Photoshop",
+                    previewImage: "images/social/1000003401.jpg",
+                    images: ["images/social/1000003401.jpg", "images/social/1000003324.png", "images/social/1000003399.jpg", "images/social/1000003398.jpg", "images/social/1000003397.jpg", "images/social/1000003400.jpg"]
+                }
                 // ... بقية العناصر كما هي ...
             ]
         },
